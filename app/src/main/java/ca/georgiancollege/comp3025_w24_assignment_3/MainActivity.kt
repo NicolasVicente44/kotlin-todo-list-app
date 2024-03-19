296d83f64d52abc42c80df4aeee1c4ef3f12b684
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-03-19 17:52:14 -0400

    started ui and recycler view for vertical layout

---
+++ app/src/main/java/ca/georgiancollege/comp3025_w24_assignment_3/MainActivity.kt
@@ -0,0 +1,14 @@
+package ca.georgiancollege.comp3025_w24_assignment_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
