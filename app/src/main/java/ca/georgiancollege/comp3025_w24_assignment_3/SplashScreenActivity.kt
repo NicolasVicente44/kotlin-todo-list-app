b1061e64823591756c07bfebea4e451c9aade607
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-03-23 15:25:50 -0400

    added splahs screen to load before the app loads for 2 seeconds

---
+++ app/src/main/java/ca/georgiancollege/comp3025_w24_assignment_3/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package ca.georgiancollege.comp3025_w24_assignment_3
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            window.navigationBarColor = getColor(R.color.mainBackGround)
+        }
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
