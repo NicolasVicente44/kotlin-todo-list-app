7acae592481f1808be64a06e78f951256a276905
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-03-23 10:34:43 -0400

    internal documentation

--- app/src/main/java/ca/georgiancollege/comp3025_w24_assignment_3/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/comp3025_w24_assignment_3/MainActivity.kt
@@ -1,5 +1,14 @@
 package ca.georgiancollege.comp3025_w24_assignment_3
 
+/**
+ * Nicolas Vicente
+ * 200539594
+ * March 3rd, 2023
+ * Main Activity file
+ *To do list UI app. This is part one of the todo list application that uses firebase and and authentication.
+ * this part is only the UI for the application with limited functionality, of which will be built upon in assignment 4
+ */
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
---
+++ app/src/main/java/ca/georgiancollege/comp3025_w24_assignment_3/ToDoItemDetailsActivity.java
@@ -0,0 +1,12 @@
+package ca.georgiancollege.comp3025_w24_assignment_3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ToDoItemDetailsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.to_do_item_details);
+    }
+}
